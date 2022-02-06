d490c5bbb8c861601a4c6f336c3eb9c00b181526
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2022-02-06 10:45:35 +0500

    ✨ feat (Home) : Add  NavigationController & ImagePicker via camera

--- ImageRecognition_ML/ViewController.swift
+++ ImageRecognition_ML/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - UIView Objects
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //ImagePicker
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //Get captured image and use it
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - IBActions
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 
 }
 
